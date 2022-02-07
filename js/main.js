2bdf9987c62e93dc633c817aa4298b4c8cf589be
zkamronbek <zoyirovkamronbek991@gmail.com>
2022-02-07 18:52:48 +0500

    initeal commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// const burger = document.querySelector (".header__burger");
+// const burger = document.querySelector (".header__nav");
+// burger.addEventListener("click", function (){
+//     burger.classList.toggle ("header__burger-opened");
+//     burger.classList.toggle ("header__nav-opened");
+// })
+const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header__nav")
+
+burger.addEventListener("click", function() {
+  burger.classList.toggle("header__burger--opened");
+  nav.classList.toggle("header__nav--opened");
+})
